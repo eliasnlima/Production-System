44933619e99644af0063bfa6c6291923f798be7c
elias <eliasnerylima018@gmail.com>
2026-02-22 21:22:19 -0300

    fix: config backend deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,10 @@
 import app from "./app.js";
+import dotenv from 'dotenv'
 
-app.listen(3030)
+dotenv.config()
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
